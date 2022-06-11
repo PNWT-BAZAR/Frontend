18ea61c7b8dd51f23bee4f0a602545816f49ce95
Taida Kadrić <tkadric1@etf.unsa.ba>
2022-06-12 00:05:39 +0200

    Completed Category view

--- src/Pages/Category/CategoryView.jsx
+++ src/Pages/Category/CategoryView.jsx
@@ -26,7 +26,7 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useNavigate } from "react-router-dom";
 import Layout from "../Layout/Layout";
 
-const drawerWidth = 240;
+const drawerWidth = "15vw";
 
 export default function CategoryView() {
   const navigate = useNavigate();
@@ -34,26 +34,37 @@ export default function CategoryView() {
     window.location.pathname.lastIndexOf("/") + 1
   );
   const [category, setCategory] = useState();
+  const [subcategories, setSubcategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [subcategoryId, setSubcategoryId] = useState("");
   useEffect(() => {
-    const fetchCategories = async () => {
+    const fetchCategory = async () => {
       const result = await API.get("/inventory/categories/" + categoryId);
       const cat = result?.data?.object;
       setCategory(cat);
     };
-    fetchCategories();
-  }, []);
-
-  const [subcategories, setSubcategories] = useState([]);
-  useEffect(() => {
     const fetchSubcategories = async () => {
       const params = new URLSearchParams([["categoryId", categoryId]]);
-      const result = await API.get("inventory/subcategories/search", {
+      const result1 = await API.get("inventory/subcategories/search", {
         params,
       });
-      setSubcategories(result?.data?.objectsList);
+      setSubcategories(result1?.data?.objectsList);
     };
+    const fetchProducts = async () => {
+      const params = new URLSearchParams([
+        ["subcategoryId", subcategoryId],
+        ["categoryId", categoryId],
+      ]);
+      const result2 = await API.get("inventory/products/search", {
+        params,
+      });
+      setProducts(result2?.data?.objectsList);
+      console.log("PRODUCTS", result2);
+    };
+    fetchCategory();
     fetchSubcategories();
-  }, []);
+    fetchProducts();
+  }, [subcategoryId]);
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -74,13 +85,23 @@ export default function CategoryView() {
         anchor="left"
       >
         <Toolbar sx={{ height: "130px" }} />
-        <Typography variant="h4" alignSelf="center" marginTop={5}>
+        <Typography
+          sx={{ cursor: "pointer" }}
+          variant="h4"
+          alignSelf="center"
+          marginTop={5}
+          onClick={(e) => setSubcategoryId("")}
+        >
           {category?.name}
         </Typography>
         <Divider />
         <List>
           {subcategories?.map((subcategory) => (
-            <ListItem key={subcategory?.id} disablePadding>
+            <ListItem
+              key={subcategory?.id}
+              disablePadding
+              onClick={(e) => setSubcategoryId(subcategory?.id)}
+            >
               <ListItemButton>
                 <ListItemIcon>
                   <ArrowForwardIosIcon />
@@ -99,14 +120,15 @@ export default function CategoryView() {
             paddingTop: "40px",
             display: "flex",
             flexWrap: "wrap",
-            width: "100%",
+            width: "85vw",
+            height: "100vh",
             justifyContent: "space-evenly",
             backgroundImage: "linear-gradient(32deg, #f3f4f7 0%, #eeeeee 90%)",
             backgroundPosition: "center",
             backgroundSize: "cover",
           }}
         >
-          {dummyProducts?.map((prod) => {
+          {products?.map((prod) => {
             return (
               <ProductCard
                 key={prod?.id}
