65de1030f332eeafe57e3519db08411f606b53c8
Taida Kadrić <tkadric1@etf.unsa.ba>
2022-06-12 16:29:27 +0200

    Added validations to register screen

--- src/Pages/LoginAndRegister/Register.jsx
+++ src/Pages/LoginAndRegister/Register.jsx
@@ -1,4 +1,4 @@
-import { Grid, TextField, InputAdornment, Button } from "@mui/material";
+import { Grid, Box, InputAdornment, Button } from "@mui/material";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
@@ -6,149 +6,261 @@ import PhoneIphoneOutlinedIcon from "@mui/icons-material/PhoneIphoneOutlined";
 import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import { FormProvider, useForm } from "react-hook-form";
+import FormInputField from "../../shared/controls/FormInput/FormInputField";
+import API from "../../api/API";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { COLORS } from "../values/colors";
+
+const schema = yup.object().shape({
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string().required("Last name is required"),
+  shippingAddress: yup.string().required("Shipping address is required"),
+  email: yup
+    .string()
+    .required("Email is required")
+    .email("E-mail is not valid!"),
+  shippingAddres: yup.string().required("Shipping address is required"),
+  username: yup
+    .string()
+    .required("Username is required")
+    .min(8, "Username must contain at least 8 characters!")
+    .max(15, "Username cannot be longer than 15 characters!"),
+  password: yup.string().required("Password is required"),
+  confirmPassword: yup.string().required("You must confirm your password!"),
+  phoneNumber: yup
+    .string()
+    .required("Phone number is required!")
+    .matches(/^[+]?\d{9,15}$/, "Phone number has to contain 9 to 15 digits!"),
+});
 
 const Register = () => {
+  const methods = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      phoneNumber: "",
+      shippingAddress: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const submitHandler = async (data) => {
+    //encrypt and login method
+    console.log("DATA", data);
+    const user = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      username: data.username,
+      password: data.password,
+      email: data.email,
+      phoneNumber: data.phoneNumber,
+      shippingAddres: data.shippingAddres,
+      role: "USER",
+    };
+    const result = await API.post("/identity/users/signup", user);
+    console.log("Signup result", result);
+    // localStorage.setItem("access_token", result?.headers?.authorization);
+  };
+
   return (
-    <Grid
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        textAlign: "center",
-      }}
-    >
-      <h2>Create an account!</h2>
-      <br />
-
-      <Grid
-        container
-        direction={"rows"}
-        spacing={5}
-        alignItems="center"
-        justifyContent="center"
+    <FormProvider {...methods}>
+      <form
+        // style={{
+        //   marginTop: "5px",
+        //   display: "flex",
+        //   alignItems: "center",
+        //   justifyContent: "center",
+        //   width: "100%",
+        //   paddingLeft: "5px",
+        //   paddingRight: "5px",
+        //   flexDirection: "column",
+        // }}
+        onSubmit={handleSubmit((data) => submitHandler(data))}
       >
-        <Grid item xs={12} sm={5} md={5}>
-          <TextField
-            label="First name"
-            placeholder="Enter first name"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <PersonOutlineOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
+        <Grid
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+          }}
+        >
+          <h2>Create an account!</h2>
+          <br />
 
-        <Grid item xs={12} sm={5} md={5}>
-          <TextField
-            label="Phone"
-            placeholder="Enter phone number"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <PhoneIphoneOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
+          <Grid
+            container
+            direction={"rows"}
+            spacing={5}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Grid item xs={12} sm={5} md={5}>
+              <FormInputField
+                name="firstName"
+                label="First name"
+                errorobj={errors}
+                placeholder="Enter first name"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PersonOutlineOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
 
-        <Grid item xs={12} sm={5} md={5}>
-          <TextField
-            label="Last name"
-            placeholder="Enter last name"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <PersonOutlineOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
+            <Grid item xs={12} sm={5} md={5}>
+              <FormInputField
+                name="phoneNumber"
+                label="Phone"
+                errorobj={errors}
+                placeholder="Enter phone number"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PhoneIphoneOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
 
-        <Grid item xs={12} sm={5} md={5}>
-          <TextField
-            label="Shipping address"
-            placeholder="Enter shipping address"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LocalShippingOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
+            <Grid item xs={12} sm={5} md={5}>
+              <FormInputField
+                name="lastName"
+                label="Last name"
+                errorobj={errors}
+                placeholder="Enter last name"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PersonOutlineOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
 
-        <Grid item xs={12} sm={5} md={5}>
-          <TextField
-            label="Username"
-            placeholder="Enter username"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AccountCircleOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
+            <Grid item xs={12} sm={5} md={5}>
+              <FormInputField
+                name="shippingAddress"
+                label="Shipping address"
+                errorobj={errors}
+                placeholder="Enter shipping address"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LocalShippingOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
 
-        <Grid item xs={12} sm={5} md={5}>
-          <TextField
-            label="Password"
-            placeholder="Enter password"
-            type="password"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LockOpenOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
+            <Grid item xs={12} sm={5} md={5}>
+              <FormInputField
+                name="username"
+                label="Username"
+                errorobj={errors}
+                placeholder="Enter username"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircleOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
 
-        <Grid item xs={12} sm={5} md={5}>
-          <TextField
-            label="E-mail"
-            placeholder="Enter e-mail"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <EmailOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
+            <Grid item xs={12} sm={5} md={5}>
+              <FormInputField
+                name="password"
+                label="Password"
+                errorobj={errors}
+                placeholder="Enter password"
+                type="password"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LockOpenOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
 
-        <Grid item xs={12} sm={5} md={5}>
-          <TextField
-            label="Confirm"
-            placeholder="Re-enter password"
-            type="password"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LockOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
-      </Grid>
+            <Grid item xs={12} sm={5} md={5}>
+              <FormInputField
+                name="email"
+                label="E-mail"
+                errorobj={errors}
+                placeholder="Enter e-mail"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <EmailOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
 
-      <Button
-        sx={{ padding: 2, minWidth: "20%", margin: "20px auto" }}
-        type="submit"
-        variant="outlined"
+            <Grid item xs={12} sm={5} md={5}>
+              <FormInputField
+                name="confirmPassword"
+                label="Confirm"
+                errorobj={errors}
+                placeholder="Re-enter password"
+                type="password"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LockOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
       >
-        Register
-      </Button>
-    </Grid>
+        <Button
+          type="submit"
+          sx={{
+            padding: "8px",
+            width: "200px",
+            margin: "20px auto",
+            color: "white",
+            backgroundColor: COLORS.primaryColor + "!important",
+          }}
+          onClick={handleSubmit((data) => submitHandler(data))}
+        >
+          Register
+        </Button>
+      </Box>
+    </FormProvider>
   );
 };
 
