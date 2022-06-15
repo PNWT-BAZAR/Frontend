7a8dda4d763f4cbf98245e856f905f928696a63f
Taida Kadrić <tkadric1@etf.unsa.ba>
2022-06-15 18:48:44 +0200

    Admin - delete users button

--- src/Pages/Admin/UsersList.jsx
+++ src/Pages/Admin/UsersList.jsx
@@ -1,39 +1,69 @@
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { dummyUserData } from "../../sampleItems/dummyUserData";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import { useState, useEffect } from "react";
 import API from "../../api/API";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 150 },
+  { field: "id", headerName: "ID", width: 100 },
   { field: "firstName", headerName: "First name", width: 150 },
   { field: "lastName", headerName: "Last name", width: 150 },
   { field: "username", headerName: "Username", width: 150 },
   { field: "email", headerName: "Email", width: 250 },
   { field: "phoneNumber", headerName: "Phone number", width: 150 },
   { field: "shippingAddress", headerName: "Shipping address", width: 250 },
-  { field: "role", headerName: "Role", width: 150 },
+  { field: "role", headerName: "Role", width: 80 },
+  {
+    field: "delete",
+    headerName: "",
+    sortable: false,
+    renderCell: () => {
+      return (
+        <DeleteOutlineOutlinedIcon
+          style={{
+            width: "100%",
+            color: "gray",
+            fontSize: 16,
+            cursor: "pointer",
+            alignSelf: "center",
+          }}
+        ></DeleteOutlineOutlinedIcon>
+      );
+    },
+  },
 ];
 
 const UsersList = () => {
   const [loading, setLoading] = useState();
   const [users, setUsers] = useState([]);
+
+  const handleCellClick = async (param, event) => {
+    if (param.field === "delete") {
+      await API.delete("/identity/users/" + param.id);
+      fetchUsers();
+    }
+    console.log("ON CLICK", param.field);
+  };
+
+  const fetchUsers = async () => {
+    const result = await API.get("identity/users", {});
+    setUsers(result?.data?.objectsList);
+    setLoading(false);
+  };
+
   useEffect(() => {
     setLoading(true);
-    const fetchUsers = async () => {
-      const result = await API.get("identity/users", {});
-      setUsers(result?.data?.objectsList);
-      setLoading(false);
-    };
     fetchUsers();
   }, []);
   return (
     <div style={{ height: "90vh", width: "100%" }}>
-      <DataGrid 
-      rows={users} 
-      columns={columns} 
-      autoPageSize={true} 
-      loading={loading}
+      <DataGrid
+        rowSelection={"single"}
+        rows={users}
+        columns={columns}
+        autoPageSize={true}
+        onCellClick={handleCellClick}
+        loading={loading}
       />
     </div>
   );
